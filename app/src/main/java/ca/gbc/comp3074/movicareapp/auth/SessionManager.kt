886caa0cff6b965b6dfd5ad4a02be5b576d45be4
jezrilcalivoso <jezril.calivoso@georgebrown.ca>
2025-11-17 15:13:21 -0500

    updated session manager to add additional states

--- app/src/main/java/ca/gbc/comp3074/movicareapp/auth/SessionManager.kt
+++ app/src/main/java/ca/gbc/comp3074/movicareapp/auth/SessionManager.kt
@@ -16,23 +16,33 @@ class SessionManager(private val context: Context) {
         private val KEY_USER_ID   = intPreferencesKey("user_id")
         private val KEY_USERNAME  = stringPreferencesKey("username")
         private val KEY_ROLE      = stringPreferencesKey("role")
+        private val KEY_STREET = stringPreferencesKey("street")
+        private val KEY_UNIT = stringPreferencesKey("unit")
+        private val KEY_POSTAL_CODE = stringPreferencesKey("postal_code")
+
     }
 
     val session = context.dataStore.data.map { pref ->
         SessionState(
-            loggedIn = pref[KEY_LOGGED_IN] ?: false,
-            userId   = pref[KEY_USER_ID],
-            username = pref[KEY_USERNAME],
-            role     = pref[KEY_ROLE]
+            loggedIn   = pref[KEY_LOGGED_IN] ?: false,
+            userId     = pref[KEY_USER_ID],
+            username   = pref[KEY_USERNAME],
+            role       = pref[KEY_ROLE],
+            street     = pref[KEY_STREET],
+            unit       = pref[KEY_UNIT],
+            postalCode = pref[KEY_POSTAL_CODE]
         )
     }
 
-    suspend fun save(userId: Int, username: String, role: String) {
+    suspend fun save(userId: Int, username: String, role: String, street: String, unit: String, postalCode: String) {
         context.dataStore.edit { p ->
-            p[KEY_LOGGED_IN] = true
-            p[KEY_USER_ID]   = userId
-            p[KEY_USERNAME]  = username
-            p[KEY_ROLE]      = role
+            p[KEY_LOGGED_IN]   = true
+            p[KEY_USER_ID]     = userId
+            p[KEY_USERNAME]    = username
+            p[KEY_ROLE]        = role
+            p[KEY_STREET]      = street
+            p[KEY_UNIT]        = unit
+            p[KEY_POSTAL_CODE] = postalCode
         }
     }
 
@@ -45,5 +55,8 @@ data class SessionState(
     val loggedIn: Boolean,
     val userId: Int?,
     val username: String?,
-    val role: String?
+    val role: String?,
+    val street: String?,
+    val unit: String?,
+    val postalCode: String?
 )
